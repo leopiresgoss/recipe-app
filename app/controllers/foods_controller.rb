04e94369406db75974f4247af3bee26371bab3bf
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-06-21 11:27:32 -0600

    Add FoodsController and update its views

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -5,8 +5,34 @@ class FoodsController < ActionController::Base
     @foods = Food.where(user: current_user)
   end
 
+  def new
+    food = Food.new
+    respond_to do |format|
+      format.html { render :new, locals: { food: } }
+    end
+  end
+
+  def create
+    food = current_user.foods.new(food_params)
+    respond_to do |format|
+      format.html do
+        if food.save
+          redirect_to foods_path, notice: 'Food added!'
+        else
+          redirect_to new_food_path, alert: 'Please, fill all fields'
+        end
+      end
+    end
+  end
+
   def destroy
     Food.find(params[:id]).destroy
     redirect_to foods_path
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price)
   end
 end
