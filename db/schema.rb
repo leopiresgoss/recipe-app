c32897208b3b6ea973feee1accdc3cd00e47ca51
Kenny <kennysalazar3197@gmail.com>
2022-06-21 12:19:43 -0500

    Create User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_20_202043) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_21_164839) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.string "preparation_time"
+    t.string "cooking_time"
+    t.string "description"
+    t.boolean "public", default: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -27,4 +39,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_20_202043) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "recipes", "users"
 end
