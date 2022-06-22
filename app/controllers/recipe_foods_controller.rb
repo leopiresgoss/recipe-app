6575602963ad93e2c8419e36c6dbc56fa9811bb7
Kenny <kennysalazar3197@gmail.com>
2022-06-22 01:34:19 -0500

    Create controllers and view for recipe_foods

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,4 +1,35 @@
-class RecipeFoods < ApplicationController
-  def infex
+class RecipeFoodsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @recipe_foods = current_user.recipes.find(params[:recipe_id])
+  end
+
+  def new
+    @recipe_food = RecipeFood.new
+  end
+
+  def create
+    recipe_food_record = RecipeFood.find_by(food_id: recipe_food_params[:food_id], recipe_id: params[:recipe_id])
+    if recipe_food_record
+      @recipe_food = recipe_food_record
+      @recipe_food.quantity += recipe_food_params[:quantity].to_i
+    else
+      @recipe_food = RecipeFood.new(**recipe_food_params, recipe_id: params[:recipe_id])
+    end
+
+    respond_to do |format|
+      if @recipe_food.save
+        format.html { redirect_to recipe_path(params[:recipe_id]), notice: 'Ingredient added.' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def recipe_food_params
+    params.require(:recipe_food).permit(:food_id, :quantity)
   end
 end
