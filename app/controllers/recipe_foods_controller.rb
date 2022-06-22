1d09590f878beafa56e4e71668b214ee35b9f81a
Kenny <kennysalazar3197@gmail.com>
2022-06-22 12:50:53 -0500

    Add ingredients table to recipes#show view

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -2,7 +2,7 @@ class RecipeFoodsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @recipe_foods = current_user.recipes.find(params[:recipe_id])
+    @recipe_foods = current_user.recipes.find(params[:recipe_id]).recipe_foods
   end
 
   def new
@@ -21,9 +21,18 @@ class RecipeFoodsController < ApplicationController
     respond_to do |format|
       if @recipe_food.save
         format.html { redirect_to recipe_path(params[:recipe_id]), notice: 'Ingredient added.' }
+        format.turbo_stream
       else
         format.html { render :new, status: :unprocessable_entity }
       end
+    end
+  end
+
+  def destroy
+    RecipeFood.find(params[:id]).destroy
+
+    respond_to do |format|
+      format.html { redirect_to recipe_path(params[:recipe_id]), notice: 'Ingredient removed.', status: :see_other }
     end
   end
 
