a37c037ccc35563b50ae04e8b5b0dfeb340638b1
Kenny <kennysalazar3197@gmail.com>
2022-06-22 13:49:53 -0500

    Add model validations

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -1,4 +1,11 @@
 class RecipeFood < ApplicationRecord
   belongs_to :recipe
   belongs_to :food
+
+  validates_associated :recipe
+  validates_associated :food
+
+  validates :food_id, :quantity, presence: true
+  validates :quantity, comparison: { greater_than_or_equal_to: 1 }
+  validates :quantity, numericality: true
 end
