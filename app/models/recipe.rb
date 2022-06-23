41da0bdd5ec79ca1aca5f9c78b3b7ebd21fbb8cb
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-06-23 13:36:13 -0600

    Add new methods to recipe_food and recipe models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,4 +6,14 @@ class Recipe < ApplicationRecord
 
   validates :name, :preparation_time, :cooking_time, :description, presence: true
   validates :preparation_time, :cooking_time, length: { maximum: 15 }
+
+  def total_price
+    recipe_foods.reduce(0) do |sum, n|
+      sum + (n.quantity * n.food.unit_price)
+    end
+  end
+
+  def total_food_items
+    recipe_foods.length
+  end
 end
