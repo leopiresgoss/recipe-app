fc1838f4f5348840e03d5db47d7bc494348155a3
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-06-24 12:30:10 -0600

    Refactor general shopping lists controller

--- app/controllers/general_shopping_lists_controller.rb
+++ app/controllers/general_shopping_lists_controller.rb
@@ -2,17 +2,20 @@ class GeneralShoppingListsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @foods = Food.joins(recipe_foods: :recipe).where(['recipes.user_id != ?', current_user.id]).map do |food|
+    @total_price = 0
+    @foods = current_user.foods.includes(:recipe_foods).map do |food|
+      next if food.recipe_foods.empty?
+
       total_quantity = food.recipe_foods.sum(:quantity)
+      price = total_quantity * food.unit_price
+      @total_price += price
 
       {
         name: food.name,
         total_quantity:,
         measurement_unit: food.measurement_unit,
-        price: total_quantity * food.unit_price
+        price:
       }
-    end
-
-    @total_price = @foods.reduce(0) { |sum, food| sum + food[:price] }
+    end.compact
   end
 end
--- spec/system/general_shopping_lists/index_spec.rb
+++ spec/system/general_shopping_lists/index_spec.rb
@@ -3,28 +3,25 @@ require 'rails_helper'
 RSpec.describe 'Public Recipes #index', type: :system do
   subject do
     test_user = User.create(name: 'Tom', email: 'tom@example.com', password: 'password')
-    Food.create(user: test_user, name: 'test_user food not used', measurement_unit: 'L', unit_price: 2)
-    corn = Food.create(user: test_user, name: 'corn', measurement_unit: 'kg', unit_price: 4)
-    soda = Food.create(user: test_user, name: 'soda', measurement_unit: 'l', unit_price: 3)
+    test_user_food = Food.create(user: test_user, name: 'test_user food', measurement_unit: 'L', unit_price: 2)
     test_recipe = Recipe.create(user: test_user, name: 'test recipe', preparation_time: '3min', cooking_time: '1min',
                                 description: 'test2')
-    RecipeFood.create(quantity: 2, food: corn, recipe: test_recipe)
-    RecipeFood.create(quantity: 3, food: soda, recipe: test_recipe)
-
-    test_user_two = User.create(name: 'Lisa', email: 'lisa@example.com', password: 'password')
-    Food.create(user: test_user_two, name: 'test_user_two food not used', measurement_unit: 'L', unit_price: 2)
-    rice = Food.create(user: test_user_two, name: 'rice', measurement_unit: 'kg', unit_price: 3)
-    milk = Food.create(user: test_user_two, name: 'milk', measurement_unit: 'l', unit_price: 1)
-    test_recipe2 = Recipe.create(user: test_user_two, name: 'test recipe', preparation_time: '3min',
-                                 cooking_time: '1min', description: 'test2')
-    RecipeFood.create(quantity: 2, food: rice, recipe: test_recipe2)
-    RecipeFood.create(quantity: 3, food: milk, recipe: test_recipe2)
+    RecipeFood.create(quantity: 2, food: test_user_food, recipe: test_recipe)
 
     @user = User.create(name: 'Maria', email: 'maria@example.com', password: 'password')
+
     egg = Food.create(user: @user, name: 'egg', measurement_unit: 'units', unit_price: 10.5)
-    user_recipe = Recipe.create(user: @user, name: 'public recipe', preparation_time: '2min',
-                                cooking_time: '5min', description: 'test')
-    RecipeFood.create(quantity: 2, food: egg, recipe: user_recipe)
+    milk = Food.create(user: @user, name: 'milk', measurement_unit: 'L', unit_price: 5)
+    Food.create(user: @user, name: 'food not used by user', measurement_unit: 'units', unit_price: 2)
+
+    user_recipe1 = Recipe.create(user: @user, name: 'public recipe', preparation_time: '2min',
+                                 cooking_time: '5min', description: 'test')
+    RecipeFood.create(quantity: 3, food: egg, recipe: user_recipe1)
+    RecipeFood.create(quantity: 3, food: milk, recipe: user_recipe1)
+
+    user_recipe2 = Recipe.create(user: @user, name: 'public recipe', preparation_time: '2min',
+                                 cooking_time: '5min', description: 'test')
+    RecipeFood.create(quantity: 4, food: milk, recipe: user_recipe2)
 
     visit new_user_session_path
     within('#new_user') do
@@ -44,53 +41,40 @@ RSpec.describe 'Public Recipes #index', type: :system do
     expect(page).to have_content('Price')
   end
 
-  it "should not display foods from the current user and the ones that other users created but didn't used them" do
+  it 'should not display foods from other users' do
     subject
     visit general_shopping_lists_path
 
-    expect(page).to_not have_content('egg')
-    expect(page).to_not have_content('test_user food not used')
-    expect(page).to_not have_content('test_user_two food not used')
+    expect(page).to_not have_content('test_user food')
+  end
+
+  it 'should not display foods that have not been used' do
+    subject
+    visit general_shopping_lists_path
+
+    expect(page).to_not have_content('food not used by user')
   end
 
   it 'should display the correct amount of food items' do
     subject
     visit general_shopping_lists_path
 
-    expect(page).to have_content('Amount of food items to buy: 4')
+    expect(page).to have_content('Amount of food items to buy: 2')
   end
 
   it 'should display the correct total price' do
     subject
     visit general_shopping_lists_path
 
-    expect(page).to have_content('Total value of food needed: $26.00')
+    expect(page).to have_content('Total value of food needed: $66.50')
   end
 
-  context 'corn' do
+  context 'egg' do
     it 'should be displayed its name, quantity and price' do
       subject
       visit general_shopping_lists_path
 
-      expect(page).to have_content('corn 2kg $8.00')
-    end
-  end
-
-  context 'soda' do
-    it 'should be displayed its name, quantity and price' do
-      subject
-      visit general_shopping_lists_path
-
-      expect(page).to have_content('soda 3l $9.00')
-    end
-  end
-
-  context 'rice' do
-    it 'should be displayed its name, quantity and price' do
-      subject
-      visit general_shopping_lists_path
-
-      expect(page).to have_content('rice 2kg $6.00')
+      expect(page).to have_content('egg 3units $31.50')
     end
   end
 
@@ -99,7 +83,7 @@ RSpec.describe 'Public Recipes #index', type: :system do
       subject
       visit general_shopping_lists_path
 
-      expect(page).to have_content('milk 3l $3.00')
+      expect(page).to have_content('milk 7L $35.00')
     end
   end
 end
